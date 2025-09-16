68f73bf8ec3cf19fc9496cbb206894c09fb4336a
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-16 16:09:58 +0100

    afficher les question initial

--- script.js
+++ script.js
@@ -1,63 +1,96 @@
 let ArrQuiz = [
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Que signifie un élément sémantique en HTML ?",
+    Reponses: ["Un élément qui donne du sens à son contenu et aide le SEO et les lecteurs automatiques",
+         "Un élément utilisé uniquement pour le style sans signification",
+          "Un élément réservé à JavaScript",
+           "Un élément qui change selon le navigateur"],
+    BonneReponse: "Un élément qui donne du sens à son contenu et aide le SEO et les lecteurs automatiques",
     NbrQst: "1/10",
   },
 
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Quel est l’élément le plus approprié pour représenter un article autonome (self-contained article) ?",
+    Reponses: ["<div>", "<article>","<span>", "<section>"],
+    BonneReponse: "<article>",
     NbrQst: "2/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Si on applique les règles suivantes au même élément : #id { color: red; } .class { color: blue; }, laquelle sera appliquée ?",
+    Reponses: [".class { color: blue; }", 
+        "#id { color: red; }",
+         "La dernière dans le fichier peu importe le type",
+          "Le navigateur choisit le sélecteur le plus court"],
+    BonneReponse: "#id { color: red; }",
     NbrQst: "3/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Quelle propriété définit l’espace intérieur (distance entre le contenu et la bordure) ?",
+    Reponses: ["margin", "border", "padding", "outline"],
+    BonneReponse: "padding",
     NbrQst: "4/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Qu’est-ce que le hoisting en JavaScript ?",
+    Reponses: ["Le déplacement des déclarations de variables et fonctions en haut de leur portée avant exécution",
+         "La désactivation des variables",
+          "Une méthode pour accélérer le chargement des images",
+           "Ajouter des propriétés au DOM uniquement"],
+    BonneReponse: "Le déplacement des déclarations de variables et fonctions en haut de leur portée avant exécution",
     NbrQst: "5/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Quelle est la différence entre == et === en JavaScript ?",
+    Reponses: ["Aucune différence", 
+        "== compare après conversion de type, tandis que === compare type et valeur (strict)",
+         "== compare uniquement le type, === compare uniquement la valeur",
+          "=== n’est disponible qu’à partir d’ES6"],
+    BonneReponse: "== compare après conversion de type, tandis que === compare type et valeur (strict)",
     NbrQst: "6/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: ") Comment déclare-t-on une variable en PHP ?",
+    Reponses: ["var x = 5", "$x = 5", "x := 5", "let x = 5"],
+    BonneReponse: "$x = 5",
     NbrQst: "7/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Qu’est-ce qu’une superglobale comme $_GET en PHP ?",
+    Reponses: ["Une variable définie uniquement dans les fonctions",
+         "Un tableau accessible dans tous les contextes (global) sans global $var",
+          "Une variable utilisable une seule fois",
+           "Une variable contenant uniquement des fonctions"],
+    BonneReponse: "Un tableau accessible dans tous les contextes (global) sans global $var",
     NbrQst: "8/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Dans quel fichier définit-on les routes web dans Laravel ?",
+    Reponses: ["routes/web.php", "app/Http/routes.php", "config/routes.php", "public/index.php"],
+    BonneReponse: "routes/web.php",
     NbrQst: "9/10",
   },
   {
-    Questionn: "",
-    Reponses: ["", "", "", ""],
-    BonneReponse: "",
+    Questionn: "Quelle est la différence principale entre routes/web.php et routes/api.php dans Laravel ?",
+    Reponses: ["Aucune différence", 
+        "web.php est utilisé pour les interfaces web avec middleware (sessions, CSRF), tandis que api.php est",
+         "api.php est réservé aux fichiers statiques",
+          "web.php ne fonctionne qu’avec JSON"],
+    BonneReponse: "web.php est utilisé pour les interfaces web avec middleware (sessions, CSRF), tandis que api.php est",
     NbrQst: "10/10",
   },
 ];
+
+let score=0;
+let NumQst=0;
+let question=document.getElementById("question");
+let choix1=document.getElementById("choix1");
+let choix2=document.getElementById("choix2");
+let choix3=document.getElementById("choix3");
+let choix4=document.getElementById("choix4");
+let nbr_question=document.getElementById("nbr_question");
+question.textContent=ArrQuiz[NumQst]. Questionn;
+choix1.textContent=ArrQuiz[NumQst].Reponses[0];
+choix2.textContent=ArrQuiz[NumQst].Reponses[1];
+choix3.textContent=ArrQuiz[NumQst].Reponses[2];
+choix4.textContent=ArrQuiz[NumQst].Reponses[3];
+nbr_question.textContent=ArrQuiz[NumQst].NbrQst;
