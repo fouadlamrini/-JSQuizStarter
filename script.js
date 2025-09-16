dd0a2e55c541cb185f59763bce2c58aa3cb3aaf8
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-16 12:24:20 +0100

    structure de QCM dans js

---
+++ script.js
@@ -0,0 +1,63 @@
+let ArrQuiz = [
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "1/10",
+  },
+
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "2/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "3/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "4/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "5/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "6/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "7/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "8/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "9/10",
+  },
+  {
+    Questionn: "",
+    Reponses: ["", "", "", ""],
+    BonneReponse: "",
+    NbrQst: "10/10",
+  },
+];
