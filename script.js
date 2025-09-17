5344b78f2ece3c4a576ec2023bb460ed5ecf7174
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-17 11:27:07 +0100

    validation de UserName et Theme

--- script.js
+++ script.js
@@ -271,3 +271,20 @@ let ObjThem = {
 // choix3.textContent=ArrQuiz[NumQst].Reponses[2];
 // choix4.textContent=ArrQuiz[NumQst].Reponses[3];
 // nbr_question.textContent=ArrQuiz[NumQst].NbrQst;
+
+let StartQuiz=document.getElementById("StartQuiz");
+
+function validation(){
+let ChoixTheme=document.getElementById("ChoixTheme");
+let input_name=document.getElementById("input_name");
+let Error=document.getElementById("Error");
+let Error1=document.getElementById("Error1");
+if(input_name.value.trim()==''){
+  Error.textContent="Entrer le nom d'utilisateur";
+  input_name.style.borderBlockColor="red";
+}
+if(ChoixTheme.value==="Theme"){
+   Error1.textContent="Choisir un theme";
+}
+
+}
