e9fa40b6b019d26ca4520eaa0e84f255d7e40eb0
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-19 09:18:20 +0100

    changer les  nom des fichier, afficher rapport

--- rapport.js
+++ rapport.js
@@ -2,3 +2,18 @@ document.getElementById('download').addEventListener('click', () => {
       const element = document.getElementById('content');
       html2pdf().from(element).save();
     });
+
+    let data = JSON.parse(localStorage.getItem("utilisateurs")) || [];
+    let user = data[data.length - 1];
+    document.querySelector(".ResCorr p span").textContent = user.score;
+document.querySelector(".Correction p span").textContent = user.theme;
+let questionDivs = document.querySelectorAll(".Correction div");
+user.answers.forEach((ans, i) => {
+  if (questionDivs[i]) {
+    let spans = questionDivs[i].querySelectorAll("span");
+
+    spans[0].textContent = ans.correctAnswer;
+
+    spans[1].textContent = ans.reponseChoisie;
+  }
+});
