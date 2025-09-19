3a10501e31e18f9b5a133d810c1c959edc61c909
fouad lmrini <fouad11.lamrini@gmail.com>
2025-09-19 11:38:46 +0100

    chnager la methode de print,change le nom des variables

--- rapport.js
+++ rapport.js
@@ -1,19 +1,29 @@
-document.getElementById('download').addEventListener('click', () => {
-      const element = document.getElementById('content');
-      html2pdf().from(element).save();
-    });
+// document.getElementById("download").addEventListener("click", () => {
+//   const element = document.getElementById("content");
+//   html2pdf().from(element).save();
+// });
 
-    let data = JSON.parse(localStorage.getItem("utilisateurs"));
-    let user = data[data.length - 1];
-    document.querySelector(".ResCorr p span").textContent = user.score;
-document.querySelector(".Correction p span").textContent = user.theme;
+document.getElementById("download").addEventListener("click", () => {
+  let download=document.getElementById("download");
+  download.style.display="none";
+  window.print();
+});
+
+let data = JSON.parse(localStorage.getItem("utilisateurs"));
+let user = data[data.length - 1];
+document.querySelector(".score").textContent = user.score;
+document.querySelector(".theme").textContent = user.theme;
+document.querySelector(".Utilisateur").textContent = user['name'];
+
 let questionDivs = document.querySelectorAll(".Correction div");
-user.answers.forEach((ans, i) => {
+
+user.answers.forEach((answer, i) => {
+  console.log(questionDivs[i]);
   if (questionDivs[i]) {
     let spans = questionDivs[i].querySelectorAll("span");
+console.log(answer);
+spans[0].textContent = answer.reponseChoisie;
+    spans[1].textContent = answer.correctAnswer;
 
-    spans[0].textContent = ans.correctAnswer;
-
-    spans[1].textContent = ans.reponseChoisie;
   }
 });
